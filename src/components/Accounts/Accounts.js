3a0c578801b17c83843f39d25095179d49322554
mekacharitha <charitha.silver@gmail.com>
2020-04-08 15:29:55 +0530

    added multiple colours for accounts card

--- src/components/Accounts/Accounts.js
+++ src/components/Accounts/Accounts.js
@@ -8,7 +8,8 @@ import Transactions from '../Transaction/Transaction';
 import { AiOutlinePlus} from "react-icons/ai";
 
 class Accounts extends Component {
-
+    backgroundColors = ["#eaf7bc","#cec3f7","#c3f7f7", "#fcc5ea","#f7b5b5", "#fc9099" , "#9eb9f7" ,"#d0ffcc"]
+        
     state={
         onDelete:false,
         divClicked:"",
@@ -36,21 +37,22 @@ class Accounts extends Component {
     render() {
         let usersAccountDetails = getAccounts();
         //let transactions=getTransactions();
-
+        let backgroundIndex = 0;
         return (
             <div>
                 <div style={{ margin: "25px" }}>
                     <div style={{ textAlign: "left", marginLeft: "25px" }}>
-                        <label style={{ fontWeight: "bold" }} >ACCOUNTS</label>
+                        <label style={{ fontWeight: "bold",fontSize:"larger" }} >ACCOUNTS</label>
                     </div>
                     <div style={{ overflowX: "auto", display: "flex" }}>
                         {usersAccountDetails.map(obj => {
-                            return (<div className="AccountCard" onClick={()=>{this.handleDivClicked(obj.accountName)}}>
+                            backgroundIndex = Math.floor(Math.random() * Math.floor(7));
+                            return (<div className="AccountCard" style={{backgroundColor:this.backgroundColors[backgroundIndex]}}  onClick={()=>{this.handleDivClicked(obj.accountName)}}>
                                 {obj.accountName}
                                 <b style={{ fontSize: "larger" }}> ₹ {obj.accountBalance} </b>
                             </div>)
                         })}
-                        <div className="AccountCard">
+                        <div className="AccountCard" style={{backgroundColor:"#aaf2c3"}}>
                             <Link to='/addAccounts'><AiOutlinePlus /></Link>
                         </div>
                     </div>
@@ -59,7 +61,7 @@ class Accounts extends Component {
                 <div style={{ marginTop: "100px" }}>
                     <div style={{ textAlign: "left", marginLeft: "50px", marginBottom: "25px" }}>
                         <label style={{ fontWeight: "bold", fontSize: "17px", marginRight: "20px" }}> RECENT TRANSACTIONS</label>
-                        <Link to="/addTransaction" className="AddTransactionButton">Add Transaction</Link>
+                        <Link to="/addTransaction" className="AddTransactionButton" >Add Transaction</Link>
                     </div>
                     <Transactions onDelete={this.handleDelete}/>
                 </div>
--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -5,6 +5,7 @@ import { FiEdit } from "react-icons/fi";
 import { MdDelete } from "react-icons/md";
 import { getTransactions } from '../../services/transactions';
 import { getAccountNameById } from '../../services/Accounts';
+import './Transaction.css';
 
 
 class Transaction extends React.Component {
@@ -20,12 +21,12 @@ class Transaction extends React.Component {
         return (
             <div style={{ marginLeft: "50px" }} >
                 {transactions.map(item => {
-                    return <div style={{ height: "50px", width: "75vw", justifyContent: "space-around", display: "flex", border: "1px solid", margin: "10px", paddingTop: "20px" }}>
-                        <div> {item.transactionType} </div>
-                        <div> {item.description} </div>
-                        <div> {item.date} </div>
-                        <div> {item.amount} </div>
-                        <div>{getAccountNameById(item.accountId)}</div>
+                    return <div style={{ height: "50px", width: "75vw", justifyContent: "space-around", display: "flex", border: "1px solid",fontSize:"20px", margin: "10px", padding: "20px" }}>
+                        <div className="TransactionItem"> {item.transactionType} </div>
+                        <div className="TransactionItem"> {item.description} </div>
+                        <div className="TransactionItem"> {item.date} </div>
+                        <div className="TransactionItem"> {item.amount} </div>
+                        <div className="TransactionItem">{getAccountNameById(item.accountId)}</div>
                         <MdDelete onClick={() => this.handleDelete(item.transactionId)} />
                         <Link to={`/editTransaction/${item.transactionId}`}><FiEdit style={{ color: "black" }} /></Link>
                     </div>
